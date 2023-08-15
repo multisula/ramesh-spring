11473207e202111fbf6a9164963eacfe599f15a1
multisula <multisula@gmail.com>
2023-08-15 23:41:02 +0900

    ModelMapper Library

--- springboot-restful-webservices/src/main/java/net/javaguides/springboot/service/impl/UserServiceImpl.java
+++ springboot-restful-webservices/src/main/java/net/javaguides/springboot/service/impl/UserServiceImpl.java
@@ -7,6 +7,7 @@ import net.javaguides.springboot.entity.User;
 import net.javaguides.springboot.mapper.UserMapper;
 import net.javaguides.springboot.repository.UserRepository;
 import net.javaguides.springboot.service.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -20,25 +21,34 @@ import java.util.stream.Collectors;
 public class UserServiceImpl implements UserService {
 
   private final UserRepository userRepository;
+  private final ModelMapper modelMapper;
 
   @Override
   public UserDto createUser(UserDto userDto) {
-    User user = UserMapper.mapToUser(userDto);
+//    User user = UserMapper.mapToUser(userDto);
+    User user = modelMapper.map(userDto, User.class);
+
     User savedUser = userRepository.save(user);
-    userDto = UserMapper.mapToUserDto(savedUser);
+
+//    userDto = UserMapper.mapToUserDto(savedUser);
+    userDto = modelMapper.map(savedUser, UserDto.class);
+
     return userDto;
   }
 
   @Override
   public UserDto getUserById(Long userId) {
     Optional<User> optionalUser = userRepository.findById(userId);
-    return UserMapper.mapToUserDto(optionalUser.get());
+//    return UserMapper.mapToUserDto(optionalUser.get());
+    return modelMapper.map(optionalUser.get(), UserDto.class);
   }
 
   @Override
   public List<UserDto> getAllUsers() {
-    return userRepository.findAll().stream().map(UserMapper::mapToUserDto).collect(Collectors.toList());
+//    return userRepository.findAll().stream().map(UserMapper::mapToUserDto).collect(Collectors.toList());
+    return userRepository.findAll().stream().map(user -> modelMapper.map(user, UserDto.class)).collect(Collectors.toList());
   }
+
 
   @Override
   @Transactional
@@ -47,7 +57,8 @@ public class UserServiceImpl implements UserService {
     existingUser.setFirstName(userDto.getFirstName());
     existingUser.setLastName(userDto.getLastName());
     existingUser.setEmail(userDto.getEmail());
-    return UserMapper.mapToUserDto(existingUser);
+//    return UserMapper.mapToUserDto(existingUser);
+    return modelMapper.map(existingUser, UserDto.class);
   }
 
   @Override
